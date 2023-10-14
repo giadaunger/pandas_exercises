32aa02a083a86a69609e6d0f5fd152cf53b0faf3
Giada Unger <giada.unger02@gmail.com>
2023-10-14 07:59:33 +0200

    Printing information from the DataFrame

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ names = ["Maria", "Adam", "Peter", "Vanessa", "Hanna"]
 
 titled_columns = {"Name": names,
                   "Height": [166, 187, 175, 168, 160],
-                  "Wight": [56, 85, 70, 63, 60]
+                  "Weight": [56, 85, 70, 63, 60]
                   }
-                  
+
 data = pd.DataFrame(titled_columns)
-print(data)
+
+
+# Printing information from the DataFrame
+select_column = data["Weight"] # Selecting a specific (by column)
+select_column = data["Weight"][2] # Selecting a specific persons weight (by column)
+select_row = data.iloc[1] # Selecting a specific (by row)
+select_row = data.iloc[1]["Height"] # Selecting a specific persons height (by row)
+# print(select_row)
