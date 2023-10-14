e6ea6ed24b9b70737dbfec52e263ed071544ad6e
Giada Unger <giada.unger02@gmail.com>
2023-10-14 07:46:09 +0200

    Added a few columns

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
 names = ["Maria", "Adam", "Peter", "Vanessa", "Hanna"]
-titeld_column = {"Name": names}
-data = pd.DataFrame(titeld_column)
+
+titled_columns = {"Name": names,
+                  "Height": [166, 187, 175, 168, 160],
+                  "Wight": [56, 85, 70, 63, 60]
+                  }
+                  
+data = pd.DataFrame(titled_columns)
 print(data)
